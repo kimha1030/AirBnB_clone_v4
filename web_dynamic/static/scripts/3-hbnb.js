92a99a82cb0ed9484bbd572ac7f399fe22800720
kimha1030 <1478@holbertonschool.com>
2020-09-28 16:28:17 +0000

    Update file task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -10,7 +10,13 @@ document.addEventListener('DOMContentLoaded', function () {
         success: function (response) {
           console.log(response);
           $.each(response, function (i, place) {
-            let valuedHTML = '<article class="site one"><div class="info"> <h2>' + place.name + '</h2> <div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest"><div class="icon_guests"></div>' + place.max_guest + ' Guests</div><div class="number_rooms"><div class="icon_bedrooms"></div>' + place.number_rooms + ' Bedroom</div><div class="number_bathrooms"><div class="icon_bathrooms"></div>' + place.number_bathrooms + ' Bathroom</div></div><div class="description"><p>' + place.description + '</p></div> </article>';
+            let valueHTML = '<article class="site one"><div class="info"> <h2>' + place.name +
+              '</h2> <div class="price_by_night">' + place.price_by_night +
+              '</div></div><div class="information"><div class="max_guest"><div class="icon_guests"></div>'
+              + place.max_guest + ' Guests</div><div class="number_rooms"><div class="icon_bedrooms"></div>'
+              + place.number_rooms + ' Bedroom</div><div class="number_bathrooms"><div class="icon_bathrooms"></div>'
+              + place.number_bathrooms + ' Bathroom</div></div><div class="description"><p>' + place.description +
+              '</p></div> </article>';
             $('.places').append(valueHTML);
           });
         }
