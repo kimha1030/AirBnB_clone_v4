39fb6fccda686b5c40f8b8a9d7f890b02bb7fa39
peluza <edisonisaza@gmail.com>
2020-09-28 19:59:25 +0000

    changes the ip addres in comunication for API

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-  $.get('http://172.17.0.2:5001/api/v1/status/', function (content) {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (content) {
     if (content.status === 'OK') {
       $.ajax({
-        url: 'http://172.17.0.2:5001/api/v1/places_search/',
-        type: "POST",
+        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        type: 'POST',
         data: '{}',
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
         success: function (response) {
           console.log(response);
           $.each(response, function (i, place) {
-            let valueHTML = '<article class="site one"><div class="info"> <h2>' + place.name +
+            const valueHTML = '<article class="site one"><div class="info"> <h2>' + place.name +
               '</h2> <div class="price_by_night">' + place.price_by_night +
-              '</div></div><div class="information"><div class="max_guest"><div class="icon_guests"></div>'
-              + place.max_guest + ' Guests</div><div class="number_rooms"><div class="icon_bedrooms"></div>'
-              + place.number_rooms + ' Bedroom</div><div class="number_bathrooms"><div class="icon_bathrooms"></div>'
-              + place.number_bathrooms + ' Bathroom</div></div><div class="description"><p>' + place.description +
+              '</div></div><div class="information"><div class="max_guest"><div class="icon_guests"></div>' +
+              place.max_guest + ' Guests</div><div class="number_rooms"><div class="icon_bedrooms"></div>' +
+              place.number_rooms + ' Bedroom</div><div class="number_bathrooms"><div class="icon_bathrooms"></div>' +
+              place.number_bathrooms + ' Bathroom</div></div><div class="description"><p>' + place.description +
               '</p></div> </article>';
             $('.places').append(valueHTML);
           });
--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -21,7 +21,7 @@ $(() => {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  $.get('http://172.17.0.2:5001/api/v1/status/', function (data) {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
     if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     }
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('DOMContentLoaded', function () {
   $('.button-filter').click(function () {
-    $.get('http://172.17.0.2:5001/api/v1/status/', function (content) {
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (content) {
       if (content.status === 'OK') {
         let result;
         const list = [];
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         $('SECTION.places').empty();
         $.ajax({
-          url: 'http://172.17.0.2:5001/api/v1/places_search/',
+          url: 'http://0.0.0.0:5001/api/v1/places_search/',
           type: 'POST',
           data: JSON.stringify({ amenities: list }),
           contentType: 'application/json; charset=utf-8',
@@ -52,12 +52,12 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log(response);
             $.each(response, function (i, place) {
               const valueHTML = '<article class="site one"><div class="info"> <h2>' + place.name +
-                                '</h2> <div class="price_by_night">' + place.price_by_night +
-                                '</div></div><div class="information"><div class="max_guest"><div class="icon_guests"></div>' +
-                                place.max_guest + ' Guests</div><div class="number_rooms"><div class="icon_bedrooms"></div>' +
-                                place.number_rooms + ' Bedroom</div><div class="number_bathrooms"><div class="icon_bathrooms"></div>' +
-                                place.number_bathrooms + ' Bathroom</div></div><div class="description"><p>' + place.description +
-                                '</p></div> </article>';
+                '</h2> <div class="price_by_night">' + place.price_by_night +
+                '</div></div><div class="information"><div class="max_guest"><div class="icon_guests"></div>' +
+                place.max_guest + ' Guests</div><div class="number_rooms"><div class="icon_bedrooms"></div>' +
+                place.number_rooms + ' Bedroom</div><div class="number_bathrooms"><div class="icon_bathrooms"></div>' +
+                place.number_bathrooms + ' Bathroom</div></div><div class="description"><p>' + place.description +
+                '</p></div> </article>';
               $('.places').append(valueHTML);
             });
           }
